75102e3fac27ac4ef04f09b43a08879d26a71475
luuvanhung <lvhdev99@gmail.com>
2021-06-10 23:09:49 +0700

    update code

--- src/page/authen/login.js
+++ src/page/authen/login.js
@@ -1,77 +1,9 @@
 import React from "react";
-import { Form, Input, Button, Row, Col } from "antd";
-import { UserOutlined, LockOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
-import LayoutPage from "../../components/Layout";
-import Directional from "../../components/Directional";
 import "./css/style.css";
+
 function LoginPage(props) {
-  const onFinish = (values) => {
-    console.log("Received values of form: ", values);
-  };
-
   return (
-    // <LayoutPage>
-    //   <>
-    //     <Directional />
-    //     <Row
-    //       style={{
-    //         padding: "100px 0px 200px",
-    //         background: "#fff",
-    //         paddingLeft: "500px",
-    //       }}
-    //     >
-    //       <Col
-    //         span={24}
-    //         style={{
-    //           maxWidth: 500,
-    //         }}
-    //       >
-    //         <Form
-    //           name="normal_login"
-    //           className="login-form"
-    //           initialValues={{ remember: true }}
-    //           onFinish={onFinish}
-    //         >
-    //           <Form.Item
-    //             name="username"
-    //             rules={[
-    //               { required: true, message: "Please input your Username!" },
-    //             ]}
-    //           >
-    //             <Input
-    //               prefix={<UserOutlined className="site-form-item-icon" />}
-    //               placeholder="Username"
-    //             />
-    //           </Form.Item>
-    //           <Form.Item
-    //             name="password"
-    //             rules={[
-    //               { required: true, message: "Please input your Password!" },
-    //             ]}
-    //           >
-    //             <Input
-    //               prefix={<LockOutlined className="site-form-item-icon" />}
-    //               type="password"
-    //               placeholder="Password"
-    //             />
-    //           </Form.Item>
-    //           <Form.Item>
-    //             <Button
-    //               type="primary"
-    //               htmlType="submit"
-    //               className="login-form-button"
-    //             >
-    //               Log in
-    //             </Button>
-    //             <Link to="/register">Register now!</Link>
-    //           </Form.Item>
-    //         </Form>
-    //       </Col>
-    //     </Row>
-    //   </>
-    // </LayoutPage>
-
     <div className="container_login">
       <div className="block_login">
         <div className="header_login">
@@ -79,12 +11,11 @@ function LoginPage(props) {
           <h2>Github Users</h2>
         </div>
         <div className="change_login">
-          <div>Log in</div>
-          <div>Sign up</div>
+          <Link to="login">Log in</Link>
+          <Link to="register">Sign up</Link>
         </div>
         <div
           className="button_login"
-           
         >
           <button style={{ marginBottom: 15 }}>
             <img src="/images/search.png" alt="" />
@@ -119,4 +50,4 @@ function LoginPage(props) {
   );
 }
 
-export default LoginPage;
+export default React.memo(LoginPage);
